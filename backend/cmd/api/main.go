28d1926209e195acf8ee305165ad31182eb77aed
Bandeira <bandeiradev.ti@gmail.com>
2026-06-08 10:26:43 -0300

    feat: integrate frontend with backend api

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"study-topics-cicd/internal/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.Default())
+
 	routes.SetupRoutes(router)
+
 	router.Run(":8080")
 }
